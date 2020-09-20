9ceb588303fd4962426b123d0bba2beb31994d62
Yasha Bubnov <ybubnov@lyft.com>
2020-09-20 14:27:03 +0300

    Test for TimeoutHandler and MultiHandler
    This patch adds unit tests for TimeoutHandler and MultiHandler.

--- hectoserver/handler_test.go
+++ hectoserver/handler_test.go
@@ -1,8 +1,13 @@
 package hectoserver
 
 import (
+	"context"
+	"errors"
 	"net"
+	"net/http"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -28,4 +33,83 @@ func TestRequest_Forward(t *testing.T) {
 			assert.Equal(t, tt.header, req.Header.Get("Forwarded"))
 		})
 	}
+}
+
+func TestTimeoutHandler(t *testing.T) {
+	var resp Response
+
+	fn := func(ctx context.Context, req *Request) (*Response, error) {
+		_, ok := ctx.Deadline()
+		assert.True(t, ok)
+		return &resp, nil
+	}
+
+	h := timeoutHandler{Handler: HandlerFunc(fn), timeout: time.Second}
+	r, err := h.Handle(context.TODO(), nil)
+
+	require.NoError(t, err)
+	assert.Equal(t, resp, *r)
+}
+
+func newHandler(id string) HandlerFunc {
+	return func(ctx context.Context, req *Request) (*Response, error) {
+		var (
+			header = make(http.Header)
+			sc     = http.StatusTemporaryRedirect
+		)
+		if id != "" {
+			header.Add("handler", id)
+			sc = http.StatusOK
+		}
+		return &Response{Header: header, StatusCode: sc}, nil
+	}
+}
+
+func TestMultiHandler_OK(t *testing.T) {
+	tests := []struct {
+		size int
+		pos  int
+	}{
+		{1, 0},
+		{2, 1},
+		{3, 1},
+		{3, 0},
+		{5, 3},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			hh := make([]Handler, tt.size)
+
+			for i := 0; i < tt.size; i++ {
+				var id string
+				if i == tt.pos {
+					id = strconv.Itoa(i)
+				}
+				hh[i] = newHandler(id)
+			}
+
+			h := MultiHandler(hh...)
+			resp, err := h.Handle(context.TODO(), new(Request))
+
+			require.NoError(t, err)
+			assert.Equal(t, strconv.Itoa(tt.pos), resp.Header.Get("handler"))
+		})
+	}
+}
+
+func TestMultiHandler_NoHandlers(t *testing.T) {
+	h := MultiHandler(newHandler(""), newHandler(""))
+	_, err := h.Handle(context.TODO(), new(Request))
+	assert.Error(t, err)
+}
+
+func TestMultiHandler_Error(t *testing.T) {
+	fn := func(ctx context.Context, req *Request) (*Response, error) {
+		return nil, errors.New("failed")
+	}
+
+	h := MultiHandler(HandlerFunc(fn))
+	_, err := h.Handle(context.TODO(), new(Request))
+	assert.Error(t, err)
 }
